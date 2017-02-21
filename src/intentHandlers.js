1f1299fa3a39c6bc99942989cf6953069f9d0f6d
Chris Lee <CMLee.1601@gmail.com>
2017-02-21 12:07:52 +0000

    Fixes #9 Delete textHelper.js

--- src/intentHandlers.js
+++ src/intentHandlers.js
@@ -1,16 +1,15 @@
 /**
-    Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 
-    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+ Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
 
-        http://aws.amazon.com/apache2.0/
+ http://aws.amazon.com/apache2.0/
 
-    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
-*/
+ or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
 
 'use strict';
-var textHelper = require('./textHelper'),
-    storage = require('./storage');
+var storage = require('./storage');
 
 var registerIntentHandlers = function (intentHandlers, skillContext) {
     intentHandlers.TellMessageIntent = function (intent, session, response) {
@@ -20,15 +19,6 @@ var registerIntentHandlers = function (intentHandlers, skillContext) {
                 speechOutput = currentMessage;
             response.tellWithCard(speechOutput, "Message", currentMessage);
         });
-    };
-
-    intentHandlers['AMAZON.HelpIntent'] = function (intent, session, response) {
-        var speechOutput = textHelper.completeHelp;
-        if (skillContext.needMoreHelp) {
-            response.ask(textHelper.completeHelp + ' So, how can I help?', 'How can I help?');
-        } else {
-            response.tell(textHelper.completeHelp);
-        }
     };
 
     intentHandlers['AMAZON.CancelIntent'] = function (intent, session, response) {
